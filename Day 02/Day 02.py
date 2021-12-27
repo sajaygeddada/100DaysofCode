a70b27f9b18ef0248e196049f62927ff32bbea92
sajaygeddada <sajaygeddada@gmail.com>
2021-12-28 03:03:50 +0530

    Day 02

---
+++ Day 02/Day 02.py
@@ -0,0 +1,30 @@
+#Data Types
+Int = 123
+Str = "Hello"
+Float = 12.34
+Boolean = True
+
+#Mathematical Operators
+# PEMDAS(L->R)
+# Parantheseis
+# Exponent
+# Multiplication
+# Division
+# Addition
+# Subtraction
+
+3 + 5
+3 - 1
+4 * 3
+6 / 3
+2 ** 3
+
+
+name = "sajay"
+age = 22
+height = 6.1
+sports = True
+
+print(f"My name is {name}, and my age is {age}, height is {height}, my sports is {sports}")
+
+print(6 + 4 / 2 - (1 * 2))
